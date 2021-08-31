aa858de7e82a326740cef1d4db7cd5df256c9325
QuanNguyen <quanna.dev@gmail.com>
2021-08-31 22:46:29 +0700

    update code

--- Youtube-GameOnlineServer/Applications/Interfaces/IWsGameServer.cs
+++
@@ -1,9 +0,0 @@
-namespace Youtube_GameOnlineServer.Applications.Interfaces
-{
-    public interface IWsGameServer
-    {
-        void StartServer();
-        void StopServer();
-        void RestartServer();
-    }
-}
